d45bcedfb0f98741412f6df49e17070aab69652d
DomDom <dom.dumont@gmail.com>
2018-10-24 14:27:13 +0200

    try to integrate jsdoc

--- src/engine/room.js
+++ src/engine/room.js
@@ -1,5 +1,9 @@
 const PIXI = require('pixi.js')
+/** This is a description of the Room
+ * @tutorial room-tutorial
+ */
 export class Room extends PIXI.Container {
+  /** This is a description of the Init function. */
   Init () {}
   Loop (delta) {}
 }
